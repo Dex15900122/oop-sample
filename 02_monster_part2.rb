9a3b0583f5006fe5933bb6bde5c3cf8a2192ab30
Dex15900122 <e5900122@yahoo.com.tw>
2018-02-26 18:37:06 +0800

    test git

--- 02_monster_part2.rb
+++ 02_monster_part2.rb
@@ -15,6 +15,19 @@ class Monster
     puts ""
   end
 
+  def monster(name, hp, ap)
+    @name = name                              # 設定一個名為 name（名稱）的 attribute
+    @hp = hp                                  # 設定一個名為 hp（生命值）的 attribute
+    @ap = ap                                  # 設定一個名為 ap（攻擊值）的 attribute
+    @alive = true                             # 怪獸剛被創造，所以預設為 true，表示怪獸創造時一定是活著的
+
+    # 印出被創造的怪獸的 attributes
+    puts "遇到怪獸 #{@name} 了!"
+    puts "生命力(HP)：#{@hp}"
+    puts "攻擊力(AP)：#{@ap}"
+    puts ""
+  end
+
   def is_alive?
     return @alive
   end
